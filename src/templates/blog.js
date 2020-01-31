ed53ab3f5191fc57a1baa603b64f715573c7a579
Eustace <joojoquartey11@gmail.com>
2020-01-31 02:56:33 +0000

    Updates

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,19 +2,9 @@ import React from 'react'
 import Layout from '../components/layout'
 import { graphql, Link } from 'gatsby'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types'
 import Head from '../components/head'
-// export const query = graphql`
-// query ($slug: String!) {
-//     markdownRemark (
-//         fields: { slug: {eq:$slug}}
-//     ) {
-//         frontmatter {
-//             title
-//             date
-//         }
-//         html
-//     }
-// }`
+import { code, date, link } from '../styles/blog.module.scss'
 
 
 export const query = graphql`
@@ -33,13 +23,25 @@ export const query = graphql`
 const Blog = (props) => {
 
     const options = {
+        renderMark: {
+            [MARKS.CODE]: text => <code className={code} >{text}</code>,
+        },
         renderNode: {
             "embedded-asset-block": (node) => {
                 const alt = node.data.target.fields && node.data.target.fields.title["en-US"]
                 const url = node.data.target.fields && node.data.target.fields.file["en-US"].url
 
                 return <img alt={alt} src={url} />
+            },
+            [INLINES.HYPERLINK]: (node) => {
+                const text = node.content && node.content[0].value.toString()
+                return <a className={link} href={node.data.uri}>{text}</a>
             }
+            // ,
+            // [BLOCKS.QUOTE]: (node, children) => {
+            //     console.log(node.content)
+            //     return <blockquote>{...children}</blockquote>
+            // }
         }
     }
 
@@ -47,9 +49,9 @@ const Blog = (props) => {
         <Layout>
             <Head title={props.data.contentfulBlogPost.title} />
             <h1>{props.data.contentfulBlogPost.title}</h1>
-            <p>{props.data.contentfulBlogPost.publishedDate}</p>
+            <p className={date}>{props.data.contentfulBlogPost.publishedDate}</p>
             {documentToReactComponents(props.data.contentfulBlogPost.postBody.json, options)}
-            <Link to="/blog">Back To Blog</Link>
+            <Link className={link} to="/blog">Back To Blog</Link>
         </Layout>
     )
 }
